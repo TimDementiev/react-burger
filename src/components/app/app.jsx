1b86ec3c342c4048f15d22b02331bb4fb7124260
Tim1380 <98985575+TimDementiev@users.noreply.github.com>
2023-02-27 23:16:00 +0300

    feat: add login, register, password, 404 pages

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -2,12 +2,24 @@ import { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { HTML5Backend } from "react-dnd-html5-backend";
 import { DndProvider } from "react-dnd";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
 
 import AppHeader from "../app-header/app-header";
 import BurgerIngredients from "../burger-ingredients/burger-ingredients";
 import BurgerConstructor from "../burger-constructor/burger-constructor";
 import appStyles from "./app.module.css";
 import { getBurgerIngredients } from "../../services/actions/burger-ingredients";
+import {
+  LoginPage,
+  RegisterPage,
+  ForgotPasswordPage,
+  ResetPasswordPage,
+  NotFound404Page,
+} from "../../pages/index";
 
 function App() {
   const dispatch = useDispatch();
@@ -21,54 +33,39 @@ function App() {
   return (
     <div className={appStyles.app}>
       <AppHeader />
-      {isLoading && "Loading"}
-      {hasError && "Error"}
-      {!isLoading && (
-        <main className={appStyles.main}>
-          <DndProvider backend={HTML5Backend}>
-            <BurgerIngredients />
-            <BurgerConstructor />
-          </DndProvider>
-        </main>
-      )}
+      <Router>
+        <Routes>
+          <Route path="/" exact forNonAuthUsers={false}>
+            {isLoading && "Loading"}
+            {hasError && "Error"}
+            {!isLoading && (
+              <main className={appStyles.main}>
+                <DndProvider backend={HTML5Backend}>
+                  <BurgerIngredients />
+                  <BurgerConstructor />
+                </DndProvider>
+              </main>
+            )}
+          </Route>
+          <Route path="/login" exact forNonAuthUsers={true}>
+            <LoginPage />
+          </Route>
+          <Route path="/register" exact forNonAuthUsers={true}>
+            <RegisterPage />
+          </Route>
+          <Route path="/forgot-password" exact forNonAuthUsers={true}>
+            <ForgotPasswordPage />
+          </Route>
+          <Route path="/reset-password" exact forNonAuthUsers={true}>
+            <ResetPasswordPage />
+          </Route>
+          <Route path="*">
+            <NotFound404Page />
+          </Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
-
-// function App() {
-//   const [state, setData] = useState({
-//     data: [],
-//     loading: true,
-//     error: false,
-//   });
-
-//   useEffect(() => {
-//     setData({ ...state, loading: true, error: false });
-//     getInitialData()
-//       .then((res) => {
-//         setData({ ...state, data: res.data, loading: false });
-//       })
-//       .catch((err) => {
-//         setData({ ...state, loading: false, error: true });
-//         console.log(err);
-//       });
-//   }, []);
-
-//   return (
-//     <div className={appStyles.app}>
-//       <AppHeader />
-//       <IngredientsContext.Provider value={state}>
-//         {state.isLoading && "Loading"}
-//         {state.hasError && "Error"}
-//         {!state.loading && (
-//           <main className={appStyles.main}>
-//             <BurgerIngredients />
-//             <BurgerConstructor />
-//           </main>
-//         )}
-//       </IngredientsContext.Provider>
-//     </div>
-//   );
-// }
 
 export default App;
